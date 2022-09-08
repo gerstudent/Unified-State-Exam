0688695c0cbdf3daa423c815c9c04f84f1de0824
gerstudent <gkolcherin@gmail.com>
2022-09-08 14:44:43 +0300

    Add files

--- for-tests/test-main.py
+++ for-tests/test-main.py
@@ -1,5 +1,9 @@
-n = int(input())
-lst = []
-for i in range(n):
-    lst.append(input().split(''))
-print(lst)
+from itertools import *
+
+word = 'ГЕПАРД'
+cnt = 0
+for x in product(word, repeat=5):
+    s = ''.join(x)
+    if s.count('Г') == 1 and s[0] != 'А' and s[-1] != 'Е':
+        cnt += 1
+print(cnt)
