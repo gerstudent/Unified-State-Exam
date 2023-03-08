5fc9929a1c4edddb48f8f61fa444ae30da3d7389
gerstudent <gkolcherin@gmail.com>
2023-03-08 16:24:47 +0300

    Update

--- 27/27data/27_test.py
+++ 27/27data/27_test.py
@@ -1,15 +1,22 @@
-with open('112/27-112a.txt') as f:
-    n, k = map(int, f.readline().split())
-    used = [0] * (10 ** 6 + 1)
-    for i in range(n):
-        used[int(f.readline())] += 1
+from numba import njit
 
-ans = 0
-for i in range(1, 10 ** 6 + 1):
-    c = 0
-    for j in range(i, 10 ** 6 + 1, i):
-        c += used[j]
-    if c >= k:
-        ans = i
 
-print(ans)
+@njit
+def calc(x, y):
+    c1 = x > 50000 and y > 50000
+    c2 = (x + y) % 80 == 0
+    return c1 and c2
+
+
+for c in ['A', 'B']:
+    with open(f'27{c}_2733.txt') as f:
+        n = int(f.readline())
+        cnt = 0
+        data = [int(x) for x in f.readlines()]
+        for i in range(n):
+            for j in range(i + 1, n):
+                c1 = data[i] > 50000 or data[j] > 50000
+                if (data[i] + data[j]) % 80 == 0 and c1:
+                    cnt += 1
+
+    print(cnt, end=' ')
